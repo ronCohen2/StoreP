d03b5084f709cafb920de173be437014f9ab69a7
ron2Cohen <ronc665@gmail.com>
2019-08-04 13:20:46 +0300

    add nongoose connect\

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import * as express from "express";
 import * as logger from "morgan";
 import * as path from "path";
-
+import * as mongoose from "mongoose";
 import { errorHandler, errorNotFoundHandler } from "./middlewares/errorHandler";
 
 // Routes
@@ -21,5 +21,15 @@ app.use(express.static(path.join(__dirname, "../public")));
 // Use routes
 app.use("/", index);
 
+//Connect to mongo
+const mongoDB: string = "mongodb://localhost/StoreDb";
+mongoose
+  .connect(mongoDB, {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  })
+  .then(() => console.log("connect to mongo"))
+  .catch(() => console.log("faild to connect to mongo"));
+  
 app.use(errorNotFoundHandler);
 app.use(errorHandler);
