3b0750a0422b21540ce5fff9bd646b9cd872af54
ronCohen2 <ronc665@gmail.com>
2019-10-11 01:41:03 +0300

    improve footer design

--- client/src/component/layout/Footer.tsx
+++ client/src/component/layout/Footer.tsx
@@ -1,30 +1,48 @@
 import React from "react";
 import { Container, Col, Row } from "reactstrap";
-
-const Footer = () => {
+import { withRouter } from "react-router";
+import { Link } from "react-router-dom";
+const Footer = (props: any) => {
+  const { history } = props;
   return (
     <div className="footer border-top">
       <Row>
         <Col sm="1" />
         <Col sm="4">
           <h4>ABOUT US</h4>
-          <p className="p-2">
+          <p className="p-2 text-muted">
             There are many variations of passages of Lorem Ipsum available, but
             There are many variations of passages of Lorem Ipsum available, but
           </p>
         </Col>
         <Col sm="3">
-          <h4>QUICK LINKS</h4>
-          <p>Home</p>
-          <p>Shoping cart</p>
-          <p>Shop</p>
-          <p>Contact</p>
+          <h4 className="text-black">QUICK LINKS</h4>
+          <div className="text-muted">
+            <p onClick={() => history.push("/")}>Home</p>
+            <p onClick={() => history.push("/shop")}>Shop</p>
+            <p onClick={() => history.push("/about")}>about</p>
+            <p onClick={() => history.push("/contact")}>Contact</p>
+          </div>
         </Col>
         <Col sm="3">
           <h4>FOLLOW US</h4>
-          <p>Facebook</p>
-          <p>Instegram</p>
-          <p>Twiter</p>
+          <div>
+            <div>
+              <a className="text-muted" href="http://www.facebook.com">
+                Facebook
+              </a>
+            </div>
+            <div>
+              <a className="text-muted" href="http://www.instegram.com">
+                Instegram
+              </a>
+            </div>
+            <div>
+              <a className="text-muted" href="http://www.twitter.com">
+                Twitter
+              </a>
+            </div>
+          </div>
         </Col>
         <Col sm="1" />
       </Row>
@@ -32,4 +50,4 @@ const Footer = () => {
   );
 };
 
-export default Footer;
+export default withRouter(Footer);
--- client/src/component/user/home/ProductCard.tsx
+++ client/src/component/user/home/ProductCard.tsx
@@ -8,14 +8,18 @@ import {
   CardSubtitle,
   Button
 } from "reactstrap";
+import { withRouter } from "react-router";
 export default function ProductCard(props: any) {
   return (
-    <div>
-      <Card className="ProductCard rounded">
+    <>
+      <Card
+        className="ProductCard rounded"
+        onClick={() => props.history.push("/shop")}
+      >
         <CardImg top width="100%" height="100%" src={props.img} />
         <CardTitle>{props.name}</CardTitle>
         <CardSubtitle>{props.price}</CardSubtitle>
       </Card>
-    </div>
+    </>
   );
 }
