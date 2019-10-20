49666007858bf24f9031e5cda85cfaf55593fb4d
ronCohen2 <ronc665@gmail.com>
2019-10-20 04:17:36 +0300

    create Verfication phone number controller

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "pug");
 
 // parse application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //
 app.use(cors());
--- src/controllers/AuthControllers.ts
+++ src/controllers/AuthControllers.ts
@@ -8,6 +8,13 @@ import { loginValidation } from "../validation/loginValidation";
 import { CartStatus } from "./CartControllers";
 const keys = require("../config/keys");
 import { isEmail } from "validator";
+const client = require("twilio");
+const Nexmo = require("nexmo");
+
+const nexmo = new Nexmo({
+  apiKey: "14e1ac25",
+  apiSecret: "tOkm3GWLwmCTEaSu"
+});
 // var twilio = require("twilio");
 
 export let registerStep1 = async (req: Request, res: Response) => {
@@ -25,7 +32,7 @@ export let registerStep1 = async (req: Request, res: Response) => {
     err.push("Email is already exist");
   }
   if (err.length > 0) {
-     return res.status(400).send(err);
+    return res.status(400).send(err);
   }
   try {
     const salt = await bcrypt.genSalt(10);
@@ -49,6 +56,7 @@ export let registerStep1 = async (req: Request, res: Response) => {
 };
 export let registerStep2 = async (req: Request, res: Response) => {
   const { fname, lname, city, street, phone, id } = req.body;
+  console.log(req.body);
   try {
     const user = await User.findByIdAndUpdate(id, {
       $set: {
@@ -68,46 +76,39 @@ export let registerStep2 = async (req: Request, res: Response) => {
 };
 
 export let registerStep3 = (req: Request, res: Response) => {
-  // const { phone, id } = req.body;
-  const rnd = Math.floor(Math.random() * 9);
-  console.log(rnd);
-  var accountSid = "ACcd57674ec7272b2c0740bca6e3844009"; // Your Account SID from www.twilio.com/console
-  var authToken = "564d3cf100ece89f12811921b9348a9e"; // Your Auth Token from www.twilio.com/console
-  const client = require("twilio")(accountSid, authToken);
-  // console.log(phone);
-  // client.messages
-  //   .create({
-  //     body: `Your key number is :${rnd}`,
-  //     to: phone, // Text this
-  //     from: "+12039413066" // From a valid Twilio number
-  //   })
-  //   .then((message: any) => console.log(message.sid));
-  // client.verify.services.create({ friendlyName: "My First Verify Service" });
-  //   .then((service: any) => {
-  //     client.verify
-  //       .services("VA9ac46fa93b2d96f7f5044c61459997c7")
-  //       .verifications.create({ to: "+9720543369400", channel: "sms" })
-  //       .then((verification: any) => console.log(verification.status));
-  //   });
-  // var accountSid = "ACcd57674ec7272b2c0740bca6e3844009"; // Your Account SID from www.twilio.com/console
-  // var authToken = "564d3cf100ece89f12811921b9348a9e"; // Your Auth Token from www.twilio.com/console
-  // const client = require("twilio")(accountSid, authToken);
-  // const sid: any = "VA9ac46fa93b2d96f7f5044c61459997c7";
-  client.verify
-    .services(`VA8a5d55faf3b6a1ef687c35f5fc1b7b6b`)
-    .verifications.create({ to: "+9720543369400", channel: "sms" })
-    .then((verification: any) => {
-      console.log(verification.status);
-      return res.status(200);
-    });
-  // .catch((err: any) => console.log(err));
+  const { phone } = req.body;
+  nexmo.verify.request(
+    {
+      number: phone,
+      brand: "sadasd",
+      code_length: "4"
+    },
+    (err: any, result: any) => {
+      if (err) {
+        console.error(err);
+        res.status(200).send("/");
+      } else {
+        const verifyRequestId = result.request_id;
+        console.log("request_id", verifyRequestId);
+      }
+    }
+  );
+  //     }
+  //   );
+  // }, 1000);
 
-  // client.verify
-  //   .services()
-  //   .verifications.create({ to: "+9720543369400", channel: "sms" })
-  //   .then((verification: any) => console.log(verification.status));
+  // ver();
 };
-
+// const ver = () => {
+//   var accountSid = "ACcd57674ec7272b2c0740bca6e3844009"; // Your Account SID from www.twilio.com/console
+//   var authToken = "3ca63a7db04297bd38ca0056371a9dd8"; // Your Auth Token from www.twilio.com/console
+//   const sid = "VA9ac46fa93b2d96f7f5044c61459997c7";
+//   client.verify
+//     .services(sid)
+//     .verifications.create({ to: "+972543369400", channel: "sms" })
+//     .then((verification: any) => console.log(verification.sid));
+// };
+// ver();
 // export let registerverification = async (req: Request, res: Response) => {
 //   const { ID, email, password, password2 } = req.body;
 //   const error = await registerValidation(req.body);
