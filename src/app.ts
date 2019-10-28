1e4390f05872b6114a648c47b0ee893fa8c13cc2
ronCohen2 <ronc665@gmail.com>
2019-10-28 11:39:55 +0200

    make it better

--- src/app.ts
+++ src/app.ts
@@ -36,7 +36,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(logger("dev"));
 
-app.use(express.static(path.join(__dirname, "../public")));
+// app.use(express.static(path.join(__dirname, "../public")));
 
 // Use routes
 app.use("/admin", Admin);
